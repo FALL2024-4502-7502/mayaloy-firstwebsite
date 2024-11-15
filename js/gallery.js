1d8870063fd02e58f86faa6cbd4ccd7bfeb194b0
mayaloy <mayakloy@gmail.com>
2024-11-14 22:25:36 -0600

    changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,43 +1,27 @@
 "use strict";
 
 var photoDivs = document.getElementsByClassName("gallery-image");
-
 var nextButton = document.getElementById("button_next");
-
 var previousButton = document.getElementById("button_previous");
-
 var photoNumber = document.getElementById("stepper");
-
 var currentPhotoNumber = 0;
 
+// Initially show the first image and set the photo number
 photoDivs[currentPhotoNumber].classList.remove('hideThis');
+photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + photoDivs.length;
-
+// Next Button - Move to the next image
 nextButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThis");
-  currentPhotoNumber = currentPhotoNumber + 1;
-
-  if (currentPhotoNumber === photoDivs.length) {
-    currentPhotoNumber = 0;
-  }
-
-  photoDivs[currentPhotoNumber].classList.remove('hideThis');
-
-  photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + photoDivs.length;
-
+    photoDivs[currentPhotoNumber].classList.add("hideThis");
+    currentPhotoNumber = (currentPhotoNumber + 1) % photoDivs.length;
+    photoDivs[currentPhotoNumber].classList.remove('hideThis');
+    photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 });
 
+// Previous Button - Move to the previous image
 previousButton.addEventListener('click', function() {
-  photoDivs[currentPhotoNumber].classList.add("hideThis");
-  currentPhotoNumber = currentPhotoNumber - 1;
-
-  if (currentPhotoNumber < 0) {
-    currentPhotoNumber = photoDivs.length - 1;
-  }
-
-  photoDivs[currentPhotoNumber].classList.remove('hideThis');
-
-  photoNumber.innerHTML = (currentPhotoNumber + 1) + " out of " + photoDivs.length;
-
+    photoDivs[currentPhotoNumber].classList.add("hideThis");
+    currentPhotoNumber = (currentPhotoNumber - 1 + photoDivs.length) % photoDivs.length;
+    photoDivs[currentPhotoNumber].classList.remove('hideThis');
+    photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 });
